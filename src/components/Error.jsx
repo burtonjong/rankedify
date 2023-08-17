f6a1aae6c8a820c7e43f289574f93da9d4af8e7c
Burton Jong <burtonjong05@gmail.com>
2023-08-16 18:11:07 -0600

    Error styling added.

--- src/components/Error.jsx
+++ src/components/Error.jsx
@@ -14,8 +14,10 @@ function Error({ setToken }) {
     <div>
       <div className="failed">
         <a href="#" onClick={logout}>
-          <h1>Invalid token.</h1>
-          <h1>Please try and login again.</h1>
+          <div className="error-message">
+            <h1 className="ta-center">Invalid token.</h1>
+            <h1>Please try and login again.</h1>
+          </div>
         </a>
       </div>
     </div>
