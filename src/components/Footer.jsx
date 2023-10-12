ce13a5d289abecaa03ad7ca6020f40bad5789931
Burton Jong <burtonjong05@gmail.com>
2023-10-11 22:53:55 -0600

    Login page changes

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,7 +1,7 @@
 function Footer() {
   return (
     <div className="footer ta-center">
-      <h4>An ongoing project by Anthony Chan and Burton Jong</h4>
+      <h4>An ongoing project by Burton Jong</h4>
     </div>
   );
 }
